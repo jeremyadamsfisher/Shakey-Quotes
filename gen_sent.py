eb22fca1b86d59dacc6468fc1944a0256596ef41
Jeremy Fisher <jeremy.adams.fisher@gmail.com>
2017-11-10 18:40:56 -0800

    Initial commit

---
+++ gen_sent.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python
+
+import csv
+import itertools
+import operator
+import numpy as np
+import nltk
+import sys
+import os
+import time
+from datetime import datetime
+from utils import *
+from rnn_theano import RNNTheano
+
+_MODEL_FILE = 'data/rnn-theano-80-5000-2017-11-10-08-01-06.npz'
+_SRC_DATA = 'data/Shakespeare_Tragedies.csv'
+
+
+_VOCABULARY_SIZE = 8000
+_HIDDEN_DIM = 80
+
+unknown_token = 'UNKNOWN_TOKEN'
+sentence_start_token = 'SENTENCE_START'
+sentence_end_token = 'SENTENCE_END'
+
+
+if _SRC_DATA.endswith('csv'):
+    print 'Reading CSV file...'
+    with open(_SRC_DATA, 'rU') as f:
+        reader = csv.reader(f, skipinitialspace=True)
+        reader.next()
+        # Split full comments into sentences
+        sentences = itertools.chain(*[nltk.sent_tokenize(x[5].decode('utf-8').lower()) for x in reader])
+        # Append SENTENCE_START and SENTENCE_END
+        sentences = ['%s %s %s' % (sentence_start_token, x, sentence_end_token) for x in sentences]
+
+print 'Parsed %d sentences.' % (len(sentences))
+
+
+# Tokenize the sentences into words
+tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
+
+
+# Count the word frequencies
+word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
+print 'Found %d unique words tokens.' % len(word_freq.items())
+
+# Get the most common words and build index_to_word and word_to_index vectors
+vocab = word_freq.most_common(_VOCABULARY_SIZE-1)
+index_to_word = [x[0] for x in vocab]
+index_to_word.append(unknown_token)
+word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
+
+model = RNNTheano(_VOCABULARY_SIZE, hidden_dim=_HIDDEN_DIM)
+load_model_parameters_theano(_MODEL_FILE, model)
+
+def generate_sentence(model):
+    # We start the sentence with the start token
+    new_sentence = [word_to_index[sentence_start_token]]
+    # Repeat until we get an end token
+    while not new_sentence[-1] == word_to_index[sentence_end_token]:
+        next_word_probs = model.forward_propagation(new_sentence)
+        sampled_word = word_to_index[unknown_token]
+        # We don't want to sample unknown words
+        while sampled_word == word_to_index[unknown_token]:
+            samples = np.random.multinomial(1, next_word_probs[-1])
+            sampled_word = np.argmax(samples)
+        new_sentence.append(sampled_word)
+    sentence_str = [index_to_word[x] for x in new_sentence[1:-1]]
+    return sentence_str
+
+num_sentences = 250
+senten_min_length = 7
+
+for i in range(num_sentences):
+    sent = []
+    # We want long sentences, not sentences with one or two words
+    while len(sent) < senten_min_length:
+        sent = generate_sentence(model)
+    print " ".join(sent).replace(' ,', ',').capitalize()
